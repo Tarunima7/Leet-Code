95ea98c23c95592b17cb973d5eb3ccfbf6269cd0
TARUNIMA SHARMA <77146630+Tarunima7@users.noreply.github.com>
2021-07-08 03:28:47 +0530

    Update Kth Smallest Element in a Sorted Matrix.cpp

--- Kth Smallest Element in a Sorted Matrix.cpp
+++ Kth Smallest Element in a Sorted Matrix.cpp
@@ -1,13 +1,21 @@
-int Solution::solve(vector<vector<int> > &A, int B) {
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& A, int B)
+    {
+      
     int n=A.size();
     int m=A[0].size();
     priority_queue<int> q;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
             if(q.size()<B)
                 q.push(A[i][j]);
-            else{
-                if(A[i][j]<q.top()){
+            else
+            {
+                if(A[i][j]<q.top())
+                {
                     q.pop();
                     q.push(A[i][j]);
                 }
@@ -15,4 +23,6 @@ int Solution::solve(vector<vector<int> > &A, int B) {
         }
     }
     return q.top();
-}
+     
+    }
+};
